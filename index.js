0a378efacb19a48b4a250e5db25788f622dab3d5
DeveloperJosh <39308600+DeveloperJosh@users.noreply.github.com>
2022-05-12 20:38:58 -0400

    ..

--- index.js
+++
@@ -1,6 +0,0 @@
-const syndb = require('./syndb.js');
-const db = new syndb('./database/db.json', false);
-db.set("new key1dsds", "new value1");
-db.create_list("new list1");
-db.push("new list1", "new value");
-db.save();
